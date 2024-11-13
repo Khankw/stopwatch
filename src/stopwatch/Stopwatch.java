1af097dfd78a529d58d1766597add2a9e7eb9168
Khankw <jskw247@gmail.com>
2024-11-13 16:06:32 +0900

    Refactor Stopwatch

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -7,22 +7,23 @@ import java.io.OutputStreamWriter;
 public class Stopwatch {
 	private Stopwatch() {
 	}
+
 	private static Stopwatch instance = new Stopwatch();
-	
+
 	public static Stopwatch getInstance() {
 		return instance;
 	}
-	
+
 	public void run() {
 		StringBuffer buffer = new StringBuffer();
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		Time time= new Time(buffer);
-		Input input= new Input(buffer);
+		Time time = new Time(buffer);
+		Input input = new Input(buffer);
 
 		Thread timerThread = new Thread(time);
 		Thread inputThread = new Thread(input);
-		
+
 		buffer.append("[q] STOP\n");
 		buffer.append("[h] HOLD\n");
 		buffer.append("[*] RERUN\n");
@@ -33,7 +34,7 @@ public class Stopwatch {
 			e.printStackTrace();
 		}
 		buffer.delete(0, buffer.length());
-		
+
 		timerThread.start();
 		inputThread.start();
 	}
