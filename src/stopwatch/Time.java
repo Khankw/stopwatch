58d6d49f4aeeaf70bde447691c39e246af6487ba
Khankw <jskw247@gmail.com>
2024-11-13 15:54:58 +0900

    Add Time

---
+++ src/stopwatch/Time.java
@@ -0,0 +1,63 @@
+package stopwatch;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Time extends Thread {
+	private StringBuffer buffer;
+
+	public Time(StringBuffer buffer) {
+		this.buffer = buffer;
+	}
+
+	@Override
+	public void run() {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		long lastTime = cal.getTimeInMillis();
+		int second = 0;
+		boolean isHold = false;
+		while (true) {
+			if (buffer.length() > 0) {
+				String input = buffer.toString();
+				if (input.equals("q"))
+					break;
+				else if (input.equals("h"))
+					isHold = true;
+				else
+					isHold = false;
+				buffer.delete(0, buffer.length());
+			}
+
+			if (isHold)
+				continue;
+
+			cal = Calendar.getInstance();
+			long curTime = cal.getTimeInMillis();
+			if (curTime - lastTime >= 1000) {
+				try {
+					writer.append(sdf.format(cal.getTime()));
+					writer.append(String.format(" [%d sec]\n", ++second));
+					writer.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				lastTime = curTime;
+			}
+		}
+		int minute = second / 60;
+		second %= 60;
+		try {
+			writer.append(String.format(">>> %d분 %d초 소요됨\n", minute, second));
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
